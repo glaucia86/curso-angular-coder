9f10801b93ff09b33da9aa087fcbdf9340878d79
Glaucia Lemos <glaucia86>
2018-06-17 19:59:10 -0300

    Revisão de TypeScript.

--- aula-7/variaveis.ts
+++ aula-7/variaveis.ts
@@ -1,19 +1,22 @@
 /**
- *
- * Arquivo: starwars.ts
+ * 
+ * Arquivo: variaveis.ts
  * Author: Glaucia Lemos
  * Description: Arquivo para revisar alguns itens sobre Typescript
  *  1) Executar o comando: Shift + Ctrl + B (Windows)
  *  2) No terminal executar: Node (nome do arquivo)
  *
  */
-var message = 'Star Wars!!';
+
+let message: string = 'Star Wars!!'
 console.log(message);
-var episodio = 4;
-console.log('Esse é o episódio 4');
-episodio = episodio + 1;
+
+let episodio: number = 4
+console.log('Esse é o episódio 4')
+
+episodio = episodio + 1
 console.log('O próximo episódio será...: ' + episodio);
-var droidFavorito;
-droidFavorito = 'BB-8';
+
+let droidFavorito: string
+droidFavorito = 'BB-8'
 console.log('Meu Droid favorito é...: ' + droidFavorito);
-//# sourceMappingURL=starwars.js.map
---
+++ aula-8/funcoes.ts
@@ -0,0 +1,9 @@
+/**
+ * 
+ * Arquivo: funcoes.ts
+ * Author: Glaucia Lemos
+ * Description: Arquivo para revisar alguns itens sobre Typescript
+ *  1) Executar o comando: Shift + Ctrl + B (Windows)
+ *  2) No terminal executar: Node (nome do arquivo)
+ *
+ */
